110641bbf2c8cb992647d92092bfc9b351283291
Justin French <justin@indent.com.au>
2008-09-17 22:42:34 +1000

    require the rspec_hpricot_matchers gem for the specs, cuz the xpath matchers frustrate me

--- spec/test_helper.rb
+++ spec/test_helper.rb
@@ -7,5 +7,9 @@ require 'action_controller'
 require 'action_view'
 require 'rexml/document'
 require File.dirname(__FILE__) + '/xpath_matchers'
+require 'rspec_hpricot_matchers'
+Spec::Runner.configure do |config|
+  config.include(RspecHpricotMatchers)
+end
 
 $LOAD_PATH.unshift(File.dirname(__FILE__) + '/../lib')
