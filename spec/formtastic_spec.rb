13e96a149ddf101e34a4a02f540eb51c825c4f61
Justin French <justin@indent.com.au>
2008-11-05 21:42:49 +1100

    added in some (commented out) specs for the boolean_select input, which fail due to an issue with rspec hijacking @template.select, i think

--- spec/formtastic_spec.rb
+++ spec/formtastic_spec.rb
@@ -823,7 +823,51 @@ describe 'Formtastic' do
     end
     
     describe 'as a boolean select input' do
-      it 'should have some specs!'
+      
+      it 'should have some specs, but rspec has hijacked the @template.select method'
+      
+      #setup do 
+      #  @new_post.stub!(:allow_comments)
+      #  @new_post.stub!(:column_for_attribute).and_return(mock('column', :type => :boolean))
+      #end
+      #
+      #it 'should have a boolean_select class on the wrapper' do
+      #  _erbout = ''
+      #  semantic_form_for(@new_post) do |builder|
+      #    _erbout += builder.input :allow_comments, :as => :boolean_select
+      #  end
+      #  _erbout.should have_tag('form li.boolean_select')
+      #end
+      #
+      #it 'should have a post_allow_comments_input id on the wrapper' do
+      #  _erbout = ''
+      #  semantic_form_for(@new_post) do |builder|
+      #    _erbout += builder.input :allow_comments, :as => :boolean_select
+      #  end
+      #  _erbout.should have_tag('form li#post_allow_comments_input')
+      #end
+      #
+      #it 'should generate a label containing the input' do
+      #  _erbout = ''
+      #  semantic_form_for(@new_post) do |builder|
+      #    _erbout += builder.input :allow_comments, :as => :boolean_select
+      #  end
+      #  _erbout.should have_tag('form li label')
+      #  _erbout.should have_tag('form li label[@for="post_allow_comments"')
+      #  _erbout.should have_tag('form li label', /Allow comments/)
+      #end
+      #
+      #it 'should generate a checkbox input' do
+      #  _erbout = ''
+      #  semantic_form_for(@new_post) do |builder|
+      #    _erbout += builder.input :allow_comments, :as => :boolean_select
+      #  end
+      #  _erbout.should have_tag('form li label input')
+      #  _erbout.should have_tag('form li label input#post_allow_comments')
+      #  _erbout.should have_tag('form li label input[@type="checkbox"]')
+      #  _erbout.should have_tag('form li label input[@name="post[allow_comments]"]')
+      #end
+      
     end
         
     describe 'as a numeric input' do
