1504eb9c8fa34c33dd274395fb1bb907a1d6de56
Negonicrac <negonicrac@gmail.com>
2009-01-07 23:21:15 -0500

    actually allow setting of html options

--- lib/justin_french/formtastic.rb
+++ lib/justin_french/formtastic.rb
@@ -44,14 +44,22 @@ module JustinFrench #:nodoc:
       [:form_for, :fields_for, :form_remote_for, :remote_form_for].each do |meth|
         src = <<-END_SRC   
           def semantic_#{meth}(record_or_name_or_array, *args, &proc)
-            options = args.extract_options!            
-            #{meth}(record_or_name_or_array, *(args << options.merge(:builder => JustinFrench::Formtastic::SemanticFormBuilder, :html => { :class => "formtastic new_" + record_or_name_or_array.class.to_s.underscore })), &proc)
+            options = args.extract_options!
+            
+            options[:builder] =  JustinFrench::Formtastic::SemanticFormBuilder
+            
+            options[:html] ||= {}
+            class_names = options[:html][:class] ? options[:html][:class].split(" ") : []
+            class_names << "formtastic"
+            class_names << record_or_name_or_array.class.to_s.underscore
+            options[:html][:class] = class_names.join(" ")
+            
+            #{meth}(record_or_name_or_array, *(args << options), &proc)
           end
         END_SRC
         module_eval src, __FILE__, __LINE__
       end
     end
- 
     
     class SemanticFormBuilder < ActionView::Helpers::FormBuilder
       
