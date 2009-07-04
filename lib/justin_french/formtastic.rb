54469762f7a9849f844b767a01c13fca2b81b7c9
Justin French <justin@indent.com.au>
2009-07-04 12:06:15 +1000

    removed the deprecated JustinFrench::* from RDoc documentation

--- lib/justin_french/formtastic.rb
+++ lib/justin_french/formtastic.rb
@@ -1,6 +1,6 @@
-module JustinFrench
-  module Formtastic
-    class SemanticFormBuilder < ::Formtastic::SemanticFormBuilder
+module JustinFrench #:nodoc:
+  module Formtastic #:nodoc:
+    class SemanticFormBuilder < ::Formtastic::SemanticFormBuilder #:nodoc:
       def initialize(*args)
         ::ActiveSupport::Deprecation.warn("JustinFrench::Formtastic::SemanticFormBuilder is deprecated. User Formtastic::SemanticFormBuilder instead", caller)
         super
