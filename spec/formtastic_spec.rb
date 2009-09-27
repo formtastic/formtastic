4828f680046393f6e9a60a00979f57d0c4336525
Justin French <justin@indent.com.au>
2009-09-28 06:28:57 +1000

    refactored the errors_on specs to better describe the functional value, including better coverage for previous commit

--- spec/formtastic_spec.rb
+++ spec/formtastic_spec.rb
@@ -338,67 +338,72 @@ describe 'Formtastic' do
       before(:each) do
         @title_errors = ['must not be blank', 'must be longer than 10 characters', 'must be awesome']
         @errors = mock('errors')
-        @errors.stub!(:[]).with(:title).and_return(@title_errors)
-        @errors.stub!(:[]).with(:body).and_return(nil)
-        @errors.stub!(:[]).with(:description).and_return([])
         @new_post.stub!(:errors).and_return(@errors)
       end
-
-      describe 'and the errors will be displayed as a sentence' do
-        it 'should render a paragraph with the errors joined into a sentence' do
+      
+      describe 'when there are errors' do
+        before do
+          @errors.stub!(:[]).with(:title).and_return(@title_errors)
+        end
+        
+        it 'should render a paragraph with the errors joined into a sentence when inline_errors config is :sentence' do
           Formtastic::SemanticFormBuilder.inline_errors = :sentence
           semantic_form_for(@new_post) do |builder|
             builder.errors_on(:title).should have_tag('p.inline-errors', @title_errors.to_sentence)
           end
         end
-      end
-
-      describe 'and the errors will be displayed as a list' do
-        it 'should render an unordered list with the class errors' do
+        
+        it 'should render an unordered list with the class errors when inline_errors config is :list' do
           Formtastic::SemanticFormBuilder.inline_errors = :list
           semantic_form_for(@new_post) do |builder|
             builder.errors_on(:title).should have_tag('ul.errors')
-          end
-        end
-
-        it 'should include a list element for each of the errors within the unordered list' do
-          Formtastic::SemanticFormBuilder.inline_errors = :list
-          semantic_form_for(@new_post) do |builder|
             @title_errors.each do |error|
               builder.errors_on(:title).should have_tag('ul.errors li', error)
             end
           end
         end
-      end
-
-      describe 'but the errors will not be shown' do
-        it 'should return nil' do
+        
+        it 'should return nil when inline_errors config is :none' do
           Formtastic::SemanticFormBuilder.inline_errors = :none
           semantic_form_for(@new_post) do |builder|
             builder.errors_on(:title).should be_nil
           end
         end
+        
       end
-
-      describe 'and no error is found on the method' do
-        it 'should return nil' do
-          Formtastic::SemanticFormBuilder.inline_errors = :sentence
-          semantic_form_for(@new_post) do |builder|
-            builder.errors_on(:body).should be_nil
+      
+      describe 'when there are no errors (nil)' do
+        before do
+          @errors.stub!(:[]).with(:title).and_return(nil)
+        end
+        
+        it 'should return nil when inline_errors config is :sentence, :list or :none' do
+          [:sentence, :list, :none].each do |config|
+            Formtastic::SemanticFormBuilder.inline_errors = config
+            semantic_form_for(@new_post) do |builder|
+              builder.errors_on(:title).should be_nil
+            end
           end
         end
       end
       
-      describe 'but the errors will not be shown' do
-        it 'should return nil' do
-          Formtastic::SemanticFormBuilder.inline_errors = :none
-          semantic_form_for(@new_post) do |builder|
-            builder.errors_on(:description).should be_nil
+      describe 'when there are no errors (empty array)' do
+        before do
+          @errors.stub!(:[]).with(:title).and_return([])
+        end
+        
+        it 'should return nil when inline_errors config is :sentence, :list or :none' do
+          [:sentence, :list, :none].each do |config|
+            Formtastic::SemanticFormBuilder.inline_errors = config
+            semantic_form_for(@new_post) do |builder|
+              builder.errors_on(:title).should be_nil
+            end
           end
         end
       end
+      
     end
-
+    
     describe '#input' do
 
       before do
