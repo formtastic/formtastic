d0d8954d55111b284569d02736e4a5a7ae382ba0
Jonas Grimfelt <grimen@gmail.com>
2009-10-01 02:13:53 +0200

    Don't generate RDoc for: JustinFrench::Formtastic::SemanticFormBuilder#initialize

--- lib/justin_french/formtastic.rb
+++ lib/justin_french/formtastic.rb
@@ -2,7 +2,7 @@
 module JustinFrench #:nodoc:
   module Formtastic #:nodoc:
     class SemanticFormBuilder < ::Formtastic::SemanticFormBuilder #:nodoc:
-      def initialize(*args)
+      def initialize(*args) #:nodoc:
         ::ActiveSupport::Deprecation.warn("JustinFrench::Formtastic::SemanticFormBuilder is deprecated. User Formtastic::SemanticFormBuilder instead", caller)
         super
       end
