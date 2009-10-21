9365444238f1b4a0905d4f986acd3cab08fb349e
Justin French <justin@indent.com.au>
2009-10-21 21:39:58 +1100

    moved custom builder stuff out to it's own spec file

--- spec/form_builder_spec.rb
+++ spec/form_builder_spec.rb
@@ -10,35 +10,6 @@ describe 'SemanticFormBuilder' do
     mock_everything
   end
 
-  describe "@@builder" do
-    before do
-      @new_post.stub!(:title)
-      @new_post.stub!(:body)
-      @new_post.stub!(:column_for_attribute).and_return(mock('column', :type => :string, :limit => 255))
-    end
-    
-    after do
-      Formtastic::SemanticFormHelper.builder = Formtastic::SemanticFormBuilder
-    end
-
-    it "can be overridden" do
-
-      class CustomFormBuilder < Formtastic::SemanticFormBuilder
-        def custom(arg1, arg2, options = {})
-          [arg1, arg2, options]
-        end
-      end
-
-      Formtastic::SemanticFormHelper.builder = CustomFormBuilder
-
-      semantic_form_for(@new_post) do |builder|
-        builder.class.should == CustomFormBuilder
-        builder.custom("one", "two").should == ["one", "two", {}]
-      end
-    end
-
-  end
-
   describe 'Formtastic::SemanticFormBuilder#semantic_fields_for' do
     before do
       @new_post.stub!(:author).and_return(::Author.new)
