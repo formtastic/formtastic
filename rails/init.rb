0963a7c583cf0d689d0dd26e29c6ecd33be4cd21
grimen <grimen@gmail.com>
2009-10-22 02:47:36 +0800

    Removed deprecated load path (lib/justinfrench/formtastic) from rails/init that raised MissingSourceFile.

--- rails/init.rb
+++ rails/init.rb
@@ -1,4 +1,3 @@
 # coding: utf-8
 require File.join(File.dirname(__FILE__), *%w[.. lib formtastic])
-require File.join(File.dirname(__FILE__), *%w[.. lib justin_french formtastic])
 ActionView::Base.send :include, Formtastic::SemanticFormHelper
