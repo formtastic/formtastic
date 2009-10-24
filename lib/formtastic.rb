formtastic/formtastic

commit d8290cf4cb521b4da541540d707fab0b1b138982

Fixed I18n label bug: "boolean_input" (a.k.a. "check_box") should use the default label method like all the other input types. (Issue #73)

lib/formtastic.rb
@@ -902,8 +902,7 @@ module Formtastic #:nodoc:
       input = self.check_box(method, set_options(options).merge(html_options),
                              options.delete(:checked_value) || '1', options.delete(:unchecked_value) || '0')
 
-      label = options.delete(:label) || humanized_attribute_name(method)
-      self.label(method, input + label, options_for_label(options))
+      self.label(method, input << self.label(method, options_for_label(options)), options_for_label(options))
     end
 
     # Generates an input for the given method using the type supplied with :as.

spec/formtastic_spec.rb
@@ -425,7 +425,11 @@ describe 'Formtastic' do
       before do
         @new_post.stub!(:title)
         @new_post.stub!(:body)
-        @new_post.stub!(:column_for_attribute).and_return(mock('column', :type => :string, :limit => 255))
+        @new_post.stub!(:published)
+        @new_post.stub!(:column_for_attribute).with(:meta_description).and_return(mock('column', :type => :string, :limit => 255))
+        @new_post.stub!(:column_for_attribute).with(:title).and_return(mock('column', :type => :string, :limit => 255))
+        @new_post.stub!(:column_for_attribute).with(:body).and_return(mock('column', :type => :text))
+        @new_post.stub!(:column_for_attribute).with(:published).and_return(mock('column', :type => :boolean))
       end
 
       describe 'with inline order customization' do
@@ -804,8 +808,10 @@ describe 'Formtastic' do
                   :formtastic => {
                       :labels => {
                         :title => @default_localized_label_text,
+                        :published => @default_localized_label_text,
                         :post => {
-                          :title => @localized_label_text
+                          :title => @localized_label_text,
+                          :published => @default_localized_label_text
                          }
                        }
                     }
@@ -815,6 +821,7 @@ describe 'Formtastic' do
               it 'should render a label with localized label (I18n)' do
                 semantic_form_for(@new_post) do |builder|
                   concat(builder.input(:title, :label => true))
+                  concat(builder.input(:published, :as => :boolean, :label => true))
                 end
                 output_buffer.should have_tag('form li label', @localized_label_text)
               end
@@ -824,12 +831,14 @@ describe 'Formtastic' do
                   :formtastic => {
                       :labels => {
                         :post => {
-                          :title => nil
+                          :title => nil,
+                          :published => nil
                          }
                        }
                     }
                 semantic_form_for(@new_post) do |builder|
                   concat(builder.input(:title, :label => true))
+                  concat(builder.input(:published, :as => :boolean, :label => true))
                 end
                 output_buffer.should have_tag('form li label', @default_localized_label_text)
               end

commit 5549c6adbee914c216949a7081c66353a2ed56c3

Resolves issue #87 (a bug introduced in d8290cf4), in which two labels were being rendered in boolean inputs.
* input_spec.rb changed to ensure only one label rendered, exposing the bug
* boolean_input now inserts the checkbox input into the options hash to pass to the label()
* label() now knows what to do with the special key in the options hash, inserting the input into the label at the last minute (after i18n, etc)

This will all probably be undone when/if we move the input outside the label (issue #60) :)

lib/formtastic.rb
@@ -374,10 +374,12 @@ module Formtastic #:nodoc:
         text = options_or_text
         options ||= {}
       end
-
       text = localized_string(method, text, :label) || humanized_attribute_name(method)
       text += required_or_optional_string(options.delete(:required))
-
+      
+      # special case for boolean (checkbox) labels, which have a nested input
+      text = (options.delete(:label_prefix_for_nested_input) || "") + text
+      
       input_name = options.delete(:input_name) || method
       if options.delete(:as_span)
         options[:class] ||= 'label'
@@ -931,8 +933,12 @@ module Formtastic #:nodoc:
 
       input = self.check_box(method, set_options(options).merge(html_options),
                              options.delete(:checked_value) || '1', options.delete(:unchecked_value) || '0')
-
-      self.label(method, input << self.label(method, options_for_label(options)), options_for_label(options))
+      options = options_for_label(options)
+      
+      # the label() method will insert this nested input into the label at the last minute
+      options[:label_prefix_for_nested_input] = input
+      
+      self.label(method, options)
     end
 
     # Generates an input for the given method using the type supplied with :as.

spec/input_spec.rb
@@ -2115,7 +2115,7 @@ describe 'SemanticFormBuilder#input' do
     end
 
     it 'should generate a label containing the input' do
-      output_buffer.should have_tag('form li label')
+      output_buffer.should have_tag('form li label', :count => 1)
       output_buffer.should have_tag('form li label[@for="post_allow_comments"]')
       output_buffer.should have_tag('form li label', /Allow comments/)
       output_buffer.should have_tag('form li label input[@type="checkbox"]')