0a71ef60becc47876d5d6f0c64b5e9d58330f3b9
Vladimir Parfinenko <vladimir.parfinenko@gmail.com>
2010-05-18 01:27:31 +0700

    Fixed misprint

--- generators/formtastic/templates/formtastic.rb
+++ generators/formtastic/templates/formtastic.rb
@@ -28,7 +28,7 @@
 # Formtastic::SemanticFormBuilder.inline_errors = :sentence
 
 # Set the method to call on label text to transform or format it for human-friendly
-# reading when formtastic is user without object. Defaults to :humanize.
+# reading when formtastic is used without object. Defaults to :humanize.
 # Formtastic::SemanticFormBuilder.label_str_method = :humanize
 
 # Set the array of methods to try calling on parent objects in :select and :radio inputs
