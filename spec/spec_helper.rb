f6ab67d2b0191072085d11ebae856cb5ac3d1520
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2010-06-29 08:49:09 +0200

    Require action_mailer in rspecs.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,6 +4,7 @@ require 'active_support'
 require 'action_pack'
 require 'action_view'
 require 'action_controller'
+require 'action_mailer'
 
 require File.expand_path(File.join(File.dirname(__FILE__), '../lib/formtastic/util'))
 require File.expand_path(File.join(File.dirname(__FILE__), '../lib/formtastic'))
