5043d432a805e021b7f3ad17f0038a7e0d887a07
Justin French <justin@indent.com.au>
2010-07-01 10:24:08 +1000

    silcence i18n deprecation warnings under Rails 2, not sure if this appropriate, but it solves the immediate problem on master

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,12 +1,16 @@
 # coding: utf-8
 require 'rubygems'
 
+gem 'i18n', '< 0.4'
+
 gem 'activesupport', '2.3.8'
 gem 'actionpack', '2.3.8'
 require 'active_support'
 require 'action_pack'
 require 'action_view'
 require 'action_controller'
+
+
 
 gem 'rspec', '>= 1.2.6'
 gem 'rspec-rails', '>= 1.2.6'
