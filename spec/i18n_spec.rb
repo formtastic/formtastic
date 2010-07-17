480e559090278cb20bf2f237ed0f8897af1840ca
Morton Jonuschat <yabawock@gmail.com>
2010-07-18 00:31:50 +0200

    Make tests use require 'spec_helper'

--- spec/i18n_spec.rb
+++ spec/i18n_spec.rb
@@ -1,5 +1,5 @@
 # coding: utf-8
-require File.join(File.dirname(__FILE__), *%w[spec_helper])
+require 'spec_helper'
 
 describe 'Formtastic::I18n' do
   
--- spec/inputs/numeric_input_spec.rb
+++ spec/inputs/numeric_input_spec.rb
@@ -1,5 +1,5 @@
 # coding: utf-8
-require File.dirname(__FILE__) + '/../spec_helper'
+require 'spec_helper'
 
 describe 'numeric input' do
   
