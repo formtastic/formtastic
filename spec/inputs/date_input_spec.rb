7767af6b8dc7f4eb55655fc87227b0461935d938
Dimitrij Denissenko <dimitrij.denissenko@blacksquaremedia.com>
2010-09-06 13:52:45 +0100

    Fix spec for non-US locales

--- spec/inputs/date_input_spec.rb
+++ spec/inputs/date_input_spec.rb
@@ -2,20 +2,20 @@
 require 'spec_helper'
 
 describe 'date input' do
-  
+
   include FormtasticSpecHelper
-  
+
   before do
     @output_buffer = ''
     mock_everything
   end
-  
+
   describe "general" do
-    
+
     before do
       output_buffer.replace ''
       @form = semantic_form_for(@new_post) do |builder|
-        concat(builder.input(:publish_at, :as => :date))
+        concat(builder.input(:publish_at, :as => :date, :order => [:year, :month, :day]))
       end
     end
 
@@ -23,12 +23,12 @@ describe 'date input' do
     it_should_have_input_wrapper_with_id("post_publish_at_input")
     it_should_have_a_nested_fieldset
     it_should_apply_error_logic_for_input_type(:date)
-    
+
     it 'should have a legend and label with the label text inside the fieldset' do
       output_buffer.concat(@form) if Formtastic::Util.rails3?
       output_buffer.should have_tag('form li.date fieldset legend.label label', /Publish at/)
     end
-    
+
     it 'should associate the legend label with the first select' do
       output_buffer.concat(@form) if Formtastic::Util.rails3?
       output_buffer.should have_tag('form li.date fieldset legend.label')
@@ -36,13 +36,13 @@ describe 'date input' do
       output_buffer.should have_tag('form li.date fieldset legend.label label[@for]')
       output_buffer.should have_tag('form li.date fieldset legend.label label[@for="post_publish_at_1i"]')
     end
-    
+
     it 'should have an ordered list of three items inside the fieldset' do
       output_buffer.concat(@form) if Formtastic::Util.rails3?
       output_buffer.should have_tag('form li.date fieldset ol')
       output_buffer.should have_tag('form li.date fieldset ol li', :count => 3)
     end
-    
+
     it 'should have three labels for year, month and day' do
       output_buffer.concat(@form) if Formtastic::Util.rails3?
       output_buffer.should have_tag('form li.date fieldset ol li label', :count => 3)
@@ -50,15 +50,15 @@ describe 'date input' do
       output_buffer.should have_tag('form li.date fieldset ol li label', /month/i)
       output_buffer.should have_tag('form li.date fieldset ol li label', /day/i)
     end
-    
+
     it 'should have three selects for year, month and day' do
       output_buffer.concat(@form) if Formtastic::Util.rails3?
       output_buffer.should have_tag('form li.date fieldset ol li select', :count => 3)
     end
   end
-    
+
   describe ':selected option' do
-    
+
     describe "when the object has a value" do
       it "should select the object value (ignoring :selected)" do
         output_buffer.replace ''
@@ -73,7 +73,7 @@ describe 'date input' do
         output_buffer.should have_tag("form li ol li select#post_created_at_1i option[@value='2012'][@selected]", :count => 1)
       end
     end
-    
+
     describe 'when the object has no value (nil)' do
       it "should select the :selected if provided as a Date" do
         output_buffer.replace ''
@@ -87,7 +87,7 @@ describe 'date input' do
         output_buffer.should have_tag("form li ol li select#post_created_at_1i option[@selected]", :count => 1)
         output_buffer.should have_tag("form li ol li select#post_created_at_1i option[@value='1999'][@selected]", :count => 1)
       end
-      
+
       it "should select the :selected if provided as a Time" do
         output_buffer.replace ''
         @new_post.stub!(:created_at => nil)
@@ -100,7 +100,7 @@ describe 'date input' do
         output_buffer.should have_tag("form li ol li select#post_created_at_1i option[@selected]", :count => 1)
         output_buffer.should have_tag("form li ol li select#post_created_at_1i option[@value='1999'][@selected]", :count => 1)
       end
-      
+
       it "should not select an option if the :selected is provided as nil" do
         output_buffer.replace ''
         @new_post.stub!(:created_at => nil)
@@ -112,7 +112,7 @@ describe 'date input' do
         output_buffer.concat(@form) if Formtastic::Util.rails3?
         output_buffer.should_not have_tag("form li ol li select#post_created_at_1i option[@selected]")
       end
-      
+
       it "should select nothing if a :selected is not provided" do
         output_buffer.replace ''
         @new_post.stub!(:created_at => nil)
@@ -121,10 +121,10 @@ describe 'date input' do
         end
         output_buffer.concat(form) if Formtastic::Util.rails3?
         output_buffer.should_not have_tag("form li ol li select option[@selected]")
-        
+
       end
     end
-    
+
   end
 
   describe ':labels option' do
@@ -164,5 +164,5 @@ describe 'date input' do
       end
     end
   end
-  
+
 end
