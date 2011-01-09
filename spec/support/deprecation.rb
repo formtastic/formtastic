adfd744de9e2ce7f9a2aed4a3b4a2e9c08bcffff
Justin French <justin@indent.com.au>
2011-01-10 07:35:11 +1100

    with_deprecation_silenced now remembers previous state, rather than assuming it was true

--- spec/support/deprecation.rb
+++ spec/support/deprecation.rb
@@ -1,6 +1,7 @@
 def with_deprecation_silenced(&block)
+  previous_value = ::ActiveSupport::Deprecation.silenced
   ::ActiveSupport::Deprecation.silenced = true
   yield
-  ::ActiveSupport::Deprecation.silenced = false
+  ::ActiveSupport::Deprecation.silenced = previous_value
 end
 
