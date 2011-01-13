2d8a53efa001476cfc4b2ac78900a100aded4427
Justin French <justin@indent.com.au>
2011-01-13 17:04:05 +1100

    we can just use ::ActiveSupport::Deprecation.silence instead

--- spec/support/deprecation.rb
+++ spec/support/deprecation.rb
@@ -1,7 +1,6 @@
 def with_deprecation_silenced(&block)
-  previous_value = ::ActiveSupport::Deprecation.silenced
-  ::ActiveSupport::Deprecation.silenced = true
-  yield
-  ::ActiveSupport::Deprecation.silenced = previous_value
+  ::ActiveSupport::Deprecation.silence do
+    yield
+  end
 end
 
