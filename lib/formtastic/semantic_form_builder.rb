f79a83efdda93f0052674473da4c715457fc773e
Gabriel Sobrinho <gabriel.sobrinho@gmail.com>
2011-01-19 23:20:26 -0200

    Formtastic::SemanticFormBuilder has been deprecated in favor of Formtastic::FormBuilder

--- lib/formtastic/semantic_form_builder.rb
+++ lib/formtastic/semantic_form_builder.rb
@@ -3,5 +3,9 @@ module Formtastic
   # TODO remove from 2.0 with a helpful upgrade path/warning.
   # @private
   class SemanticFormBuilder < Formtastic::FormBuilder
+    def initialize(*args)
+      ActiveSupport::Deprecation.warn('Formtastic::SemanticFormBuilder has been deprecated in favor of Formtastic::FormBuilder.', caller)
+      super
+    end
   end
 end
