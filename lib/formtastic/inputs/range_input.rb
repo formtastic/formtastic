003009cceba910e1b87e22801e6c52048c6c9fbe
Jakub Okoński <kuba@okonski.org>
2011-01-31 14:21:55 +0100

    Prettier code for RangeInput, allow for :step option.

--- lib/formtastic/inputs/range_input.rb
+++ lib/formtastic/inputs/range_input.rb
@@ -7,21 +7,24 @@ module Formtastic
         reflections = @object.class.reflect_on_validations_for(method) if @object.class.respond_to?(:reflect_on_validations_for)
         reflections.each do |reflection|
           if reflection.macro == :validates_numericality_of
-            if reflection.options.include?(:greater_than)
-              range_start = (reflection.options[:greater_than] + 1)
-            elsif reflection.options.include?(:greater_than_or_equal_to)
-              range_start = reflection.options[:greater_than_or_equal_to]
-            end
-            if reflection.options.include?(:less_than)
-              range_end = (reflection.options[:less_than] - 1)
-            elsif reflection.options.include?(:less_than_or_equal_to)
-              range_end = reflection.options[:less_than_or_equal_to]
+            unless options.include? :in
+              if reflection.options.include?(:greater_than)
+                range_start = (reflection.options[:greater_than] + 1)
+              elsif reflection.options.include?(:greater_than_or_equal_to)
+                range_start = reflection.options[:greater_than_or_equal_to]
+              end
+              if reflection.options.include?(:less_than)
+                range_end = (reflection.options[:less_than] - 1)
+              elsif reflection.options.include?(:less_than_or_equal_to)
+                range_end = reflection.options[:less_than_or_equal_to]
+              end
             end
             options[:input_html] ||= {}
-            options[:input_html][:in] = options.delete(:in) if options[:in]
-            options[:input_html][:in] ||= (range_start..range_end)
+            options[:input_html][:in] = options.delete(:in) || (range_start..range_end)
+            options[:input_html][:step] = options.delete(:step) || 1
           end
         end
+        
         basic_input_helper(:range_field, :numeric, method, options)
       end
   
