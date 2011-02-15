814490fc910f8936f9705f4447a6386fa995f862
Justin French <justin@indent.com.au>
2011-02-15 17:34:48 +1100

    LabelHelper documentation

--- lib/formtastic/helpers/label_helper.rb
+++ lib/formtastic/helpers/label_helper.rb
@@ -5,25 +5,60 @@ module Formtastic
     module LabelHelper
       include Formtastic::LocalizedString
       
-      # Generates the label for the input. It also accepts the same arguments as
-      # Rails label method. It has three options that are not supported by Rails
-      # label method:
+      # Generates the label for the input. It's a superset of the functionality of Rails' own `label`
+      # helper, meaning that it accepts the same arguments and options, plus some new arguments that
+      # are exclusive to Formtastic.
       #
-      # * :required - Appends an abbr tag if :required is true
-      # * :label - An alternative form to give the label content. Whenever label
-      #            is false, a blank string is returned.
-      # * :input_name - Gives the input to match for. This is needed when you want to
-      #                 to call f.label :authors but it should match :author_ids.
+      # It's unlikely you will need call this method direct from your views, but it's been made 
+      # public to document the changes made to the Rails method, for the rare occasions when you 
+      # might build your own input in the view using regular form helpers.
       #
-      # == Examples
+      # @param [Symbol] method
+      #   The method or attribute on the model this label is for (eg `:title` on a `Post`)
       #
-      #  f.label :title # like in rails, except that it searches the label on I18n API too
+      # @param [String, Hash] options_or_text
+      #   A string for the text to appear as the content of the `<label>` tag, or a Hash of options (see below)
       #
-      #  f.label :title, "Your post title"
-      #  f.label :title, :label => "Your post title" # Added for formtastic API
+      # @param [Hash] options
+      #   A Hash of options (see below)
       #
-      #  f.label :title, :required => true # Returns <label>Title<abbr title="required">*</abbr></label>
+      # @option options [String] :label
+      #   Override the text for the contents of the `<label>` tag
       #
+      # @option options [true, false] :required
+      #   Specify if the field required or not. By default this value is "guessed" by reflecting on 
+      #   the model validations, with a fallback to `all_fields_required_by_default` configuration. 
+      #   Required and optional labels have the `required_string` (defaults to an asterix wrapped 
+      #   in an `<abbr>` tag) or `optional_string` configuration variables appended to them.
+      #
+      # @option options [Symbol] :input_name
+      #   Gives the input to match for. This is needed when you want to call `f.label :authors` but 
+      #   it should match `:author_ids`.
+      #
+      #
+      # @example Basic usage, just like Rails (except that it searches i18n for the label text too)
+      #   <%= semantic_form_for @post do |f| %>
+      #     <%= f.label :title %>
+      #   <% end %>
+      #
+      # @example Basic usage with string for label text, just like Rails
+      #   <%= semantic_form_for @post do |f| %>
+      #     <%= f.label :title, "Your title" %>
+      #   <% end %>
+      #
+      # @example Alternative for label text using `:label` option to match the rest of the Formtastic API
+      #   <%= semantic_form_for @post do |f| %>
+      #     <%= f.label :title, :label => "Your title" %>
+      #   <% end %>
+      #
+      # @example Mark the label as required or not, regardless of DB, validations and default config.
+      #   <%= semantic_form_for @post do |f| %>
+      #     <%= f.label :title, :required => true %>
+      #     <%= f.label :body, :required => false %>
+      #   <% end %>
+      #
+      #
+      # @todo i18n documentation
       def label(method, options_or_text=nil, options=nil)
         if options_or_text.is_a?(Hash)
           return "" if options_or_text[:label] == false
