20aa457f925b665139249e51b1fdaf652f12cff0
Justin French <justin@indent.com.au>
2011-02-22 07:48:27 +1100

    documented Timeish, DateInput, TimeInput, DateTimeInput

--- lib/formtastic/inputs/time_input.rb
+++ lib/formtastic/inputs/time_input.rb
@@ -2,16 +2,9 @@ require 'inputs/timeish'
 
 module Formtastic
   module Inputs
+    # @see Formtastic::Inputs::Timeish Timeish module for documetation of date, time and datetime input options.
     module TimeInput
       include Formtastic::Inputs::Timeish
-      # Outputs a fieldset with a legend for the method label, and a ordered list (ol) of list
-      # items (li), one for each fragment for the time (hour, minute, second).  Each li contains a label
-      # (eg "Hour") and a select box. Overwriting the label is possible by adding the :labels option.
-      # :labels should be a hash with the field (e.g. day) as key and the label text as value.
-      # See date_or_datetime_input for a more detailed output example.
-      #
-      # Some of Rails' options for select_time are supported, but not everything yet, see
-      # documentation of date_or_datetime_input() for more information.
       def time_input(method, options)
         options = set_include_blank(options)
         date_or_datetime_input(method, options.merge(:discard_year => true, :discard_month => true, :discard_day => true))
