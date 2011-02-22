914677b5a829229b6d2fec6b0c41dd856e1f106a
Justin French <justin@indent.com.au>
2011-02-23 07:17:52 +1100

    documented StringInput

--- lib/formtastic/inputs/string_input.rb
+++ lib/formtastic/inputs/string_input.rb
@@ -2,10 +2,36 @@ require 'inputs/basic'
 
 module Formtastic
   module Inputs
+    
+    # Outputs a simple `<label>` with a `<input type="text">` wrapped in the standard
+    # `<li>` wrapper. This is the default input choice for database columns of the `:string` type,
+    # and is the default choice for all inputs when no other logical input type can be inferred.
+    # You can force any input to be a string input with `:as => :string`.
+    #
+    # @example Full form context and output
+    # 
+    #   <%= semantic_form_for(@user) do |f| %>
+    #     <%= f.inputs do %>
+    #       <%= f.input :first_name, :as => :string %>
+    #     <% end %>
+    #   <% end %>
+    #
+    #   <form...>
+    #     <fieldset>
+    #       <ol>
+    #         <li class="string">
+    #           <label for="user_first_name">First name</label>
+    #           <input type="text" id="user_first_name" name="user[first_name]">
+    #         </li>
+    #       </ol>
+    #     </fieldset>
+    #   </form>
+    #
+    # @see Formtastic::Helpers::InputsHelper#input InputsHelper#input for full documetation of all possible options.
     module StringInput
       include Formtastic::Inputs::Base
       include Formtastic::Inputs::Basic
-      # Outputs a label and standard Rails text field inside the wrapper.
+
       def string_input(method, options)
         basic_input_helper(:text_field, :string, method, options)
       end
