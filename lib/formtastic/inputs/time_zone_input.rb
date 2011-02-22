7dbce96353d1df7047460244a50b25ee54068c91
Justin French <justin@indent.com.au>
2011-02-23 07:18:15 +1100

    documented TimeZoneInput

--- lib/formtastic/inputs/time_zone_input.rb
+++ lib/formtastic/inputs/time_zone_input.rb
@@ -1,12 +1,34 @@
 module Formtastic
   module Inputs
+    
+    # Outputs a `<label>` with a `<select>` containing a series of time zones (using Rails' own 
+    # `time_zone_select` helper), wrapped in the standard `<li>` wrapper.
+    
+    # This is the default input choice for attributes matching /time_zone/, but can be applied to
+    # any text-like input with `:as => :time_zone`.
+    #
+    # @example Full form context and output
+    # 
+    #   <%= semantic_form_for(@user) do |f| %>
+    #     <%= f.inputs do %>
+    #       <%= f.input :time_zone, :as => :time_zone %>
+    #     <% end %>
+    #   <% end %>
+    #
+    #   <form...>
+    #     <fieldset>
+    #       <ol>
+    #         <li class="time_zone">
+    #           <label for="user_time_zone">Time zone</label>
+    #           <input type="text" id="user_time_zone" name="user[time_zone]">
+    #         </li>
+    #       </ol>
+    #     </fieldset>
+    #   </form>
+    #
+    # @see Formtastic::Helpers::InputsHelper#input InputsHelper#input for full documetation of all possible options.
+    
     module TimeZoneInput
-      # Outputs a timezone select input as Rails' time_zone_select helper. You
-      # can give priority zones as option.
-      #
-      # Examples:
-      #
-      #   f.input :time_zone, :as => :time_zone, :priority_zones => /Australia/
       def time_zone_input(method, options)
         html_options = options.delete(:input_html) || {}
         field_id = generate_html_id(method, "")
