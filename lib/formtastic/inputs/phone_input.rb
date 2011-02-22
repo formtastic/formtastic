6d71d3a2cd2b94d2fbc95fcd2f8012f11c33c0bd
Justin French <justin@indent.com.au>
2011-02-23 07:17:30 +1100

    documented PhoneInput

--- lib/formtastic/inputs/phone_input.rb
+++ lib/formtastic/inputs/phone_input.rb
@@ -2,6 +2,31 @@ require 'inputs/basic'
 
 module Formtastic
   module Inputs
+    
+    # Outputs a simple `<label>` with a HTML5 `<input type="phone">` wrapped in the standard
+    # `<li>` wrapper. This is the default input choice for attributes with a name matching 
+    # `/(phone|fax)/`, but can be applied to any text-like input with `:as => :phone`.
+    #
+    # @example Full form context and output
+    # 
+    #   <%= semantic_form_for(@user) do |f| %>
+    #     <%= f.inputs do %>
+    #       <%= f.input :mobile, :as => :phone %>
+    #     <% end %>
+    #   <% end %>
+    #
+    #   <form...>
+    #     <fieldset>
+    #       <ol>
+    #         <li class="phone">
+    #           <label for="user_mobile">Mobile</label>
+    #           <input type="phone" id="user_mobile" name="user[mobile]">
+    #         </li>
+    #       </ol>
+    #     </fieldset>
+    #   </form>
+    #
+    # @see Formtastic::Helpers::InputsHelper#input InputsHelper#input for full documetation of all possible options.
     module PhoneInput
       include Formtastic::Inputs::Base
       include Formtastic::Inputs::Basic
