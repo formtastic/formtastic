b917145d90d54605290e1b2378ce026eaf47ad2c
Justin French <justin@indent.com.au>
2011-02-23 07:18:26 +1100

    documented UrlInput

--- lib/formtastic/inputs/url_input.rb
+++ lib/formtastic/inputs/url_input.rb
@@ -1,9 +1,33 @@
 module Formtastic
   module Inputs
+    
+    # Outputs a simple `<label>` with a HTML5 `<input type="url">` wrapped in the standard
+    # `<li>` wrapper. This is the default input choice for all attributes matching 
+    # `/^url$|^website$|_url$/`, but can be applied to any text-like input with `:as => :url`.
+    #
+    # @example Full form context and output
+    # 
+    #   <%= semantic_form_for(@user) do |f| %>
+    #     <%= f.inputs do %>
+    #       <%= f.input :home_page, :as => :url %>
+    #     <% end %>
+    #   <% end %>
+    #
+    #   <form...>
+    #     <fieldset>
+    #       <ol>
+    #         <li class="url">
+    #           <label for="user_home_page">Home page</label>
+    #           <input type="number" id="user_home_page" name="user[home_page]">
+    #         </li>
+    #       </ol>
+    #     </fieldset>
+    #   </form>
+    #
+    # @see Formtastic::Helpers::InputsHelper#input InputsHelper#input for full documetation of all possible options.
     module UrlInput
       include Formtastic::Inputs::Basic
       
-      # Outputs a label and a standard Rails url field inside the wrapper.
       def url_input(method, options)
         basic_input_helper(:url_field, :url, method, options)
       end
