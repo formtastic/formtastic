ab5e4105fe1f1a9a8837c6c2f3eef8dcf586b048
Justin French <justin@indent.com.au>
2011-03-31 07:20:37 +1100

    shifted error_html methods to Errors module

--- lib/formtastic/inputs/new_base/errors.rb
+++ lib/formtastic/inputs/new_base/errors.rb
@@ -2,7 +2,34 @@ module Formtastic
   module Inputs
     module NewBase
       module Errors
-
+        
+        def error_html
+          errors? ? send(:"error_#{builder.inline_errors}_html") : ""
+        end
+        
+        def error_sentence_html
+          error_class = options[:error_class] || builder.default_inline_error_class
+          template.content_tag(:p, Formtastic::Util.html_safe(errors.to_sentence.html_safe), :class => error_class)
+        end
+                
+        def error_list_html
+          error_class = options[:error_class] || builder.default_error_list_class
+          list_elements = []
+          errors.each do |error|
+            list_elements << template.content_tag(:li, Formtastic::Util.html_safe(error.html_safe))
+          end
+          template.content_tag(:ul, Formtastic::Util.html_safe(list_elements.join("\n")), :class => error_class)
+        end
+        
+        def error_first_html
+          error_class = options[:error_class] || builder.default_inline_error_class
+          template.content_tag(:p, Formtastic::Util.html_safe(errors.first.untaint), :class => error_class)
+        end
+        
+        def error_none_html
+          ""
+        end
+        
         def errors?
           !errors.blank?
         end
--- lib/formtastic/inputs/new_base/html.rb
+++ lib/formtastic/inputs/new_base/html.rb
@@ -21,33 +21,6 @@ module Formtastic
           options[:input_html].try(:[], :id) || dom_id
         end
         
-        def error_html
-          errors? ? send(:"error_#{builder.inline_errors}_html") : ""
-        end
-        
-        def error_sentence_html
-          error_class = options[:error_class] || builder.default_inline_error_class
-          template.content_tag(:p, Formtastic::Util.html_safe(errors.to_sentence.html_safe), :class => error_class)
-        end
-                
-        def error_list_html
-          error_class = options[:error_class] || builder.default_error_list_class
-          list_elements = []
-          errors.each do |error|
-            list_elements << template.content_tag(:li, Formtastic::Util.html_safe(error.html_safe))
-          end
-          template.content_tag(:ul, Formtastic::Util.html_safe(list_elements.join("\n")), :class => error_class)
-        end
-        
-        def error_first_html
-          error_class = options[:error_class] || builder.default_inline_error_class
-          template.content_tag(:p, Formtastic::Util.html_safe(errors.first.untaint), :class => error_class)
-        end
-        
-        def error_none_html
-          ""
-        end
-        
         def hint_html
           if hint?
             template.content_tag(
