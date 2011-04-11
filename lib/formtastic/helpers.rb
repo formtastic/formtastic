1ebd2e84746926f0a710d2090dfcbdaca7b5a60c
Justin French <justin@indent.com.au>
2011-04-11 21:05:13 +1000

    split input() related methods out of InputsHelper into InputHelper

--- lib/formtastic/helpers.rb
+++ lib/formtastic/helpers.rb
@@ -5,6 +5,7 @@ module Formtastic
     autoload :FieldsetWrapper, 'formtastic/helpers/fieldset_wrapper'
     autoload :FileColumnDetection, 'formtastic/helpers/file_column_detection'
     autoload :FormHelper, 'formtastic/helpers/form_helper'
+    autoload :InputHelper, 'formtastic/helpers/input_helper'
     autoload :InputsHelper, 'formtastic/helpers/inputs_helper'
     autoload :LabelHelper, 'formtastic/helpers/label_helper'
     autoload :SemanticFormHelper, 'formtastic/helpers/semantic_form_helper'
