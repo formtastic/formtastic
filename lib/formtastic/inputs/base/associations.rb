3fb514776224da02d5d392961023d48dfc13addf
DeLynn Berry <delynn@gmail.com>
2011-04-17 23:58:39 -0600

    Make sure the method attribute is returned when an association doesn't exist.

--- lib/formtastic/inputs/base/associations.rb
+++ lib/formtastic/inputs/base/associations.rb
@@ -7,21 +7,23 @@ module Formtastic
         def association
           @association ||= reflection.macro if reflection
         end
-        
+
         def reflection
           @reflection ||= object.class.reflect_on_association(method) if object.class.respond_to?(:reflect_on_association)
         end
-        
+
         def belongs_to?
           association == :belongs_to
         end
-        
+
         def association_primary_key
           if association
             return reflection.options[:foreign_key] unless reflection.options[:foreign_key].blank?
             return reflection.foreign_key if reflection.respond_to?(:foreign_key)
             return :"#{method}_id" if belongs_to?
             return "#{method.to_s.singularize}_id".pluralize.to_sym
+          else
+            return method.to_s
           end
         end
 
