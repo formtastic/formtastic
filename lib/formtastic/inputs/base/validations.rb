cb7dea4ad6494c06b080c4301e0f0d7c9b6d4899
Aditya Sanghi <asanghi@me.com>
2011-05-28 12:29:55 +0530

    boolean fields should not respond to a global required default. it forces the boolean to be checked. Fixes #578

--- lib/formtastic/inputs/base/validations.rb
+++ lib/formtastic/inputs/base/validations.rb
@@ -2,7 +2,7 @@ module Formtastic
   module Inputs
     module Base
       module Validations
-        
+
         class IndeterminableMinimumAttributeError < ArgumentError
           def message
             [
@@ -20,9 +20,9 @@ module Formtastic
             ].join("\n")
           end
         end
-        
+
         def validations
-          @validations ||= if object && object.class.respond_to?(:validators_on) 
+          @validations ||= if object && object.class.respond_to?(:validators_on)
             object.class.validators_on(attributized_method_name).select do |validator|
               validator_relevant?(validator)
             end
@@ -30,11 +30,11 @@ module Formtastic
             []
           end
         end
-        
+
         def validator_relevant?(validator)
           return true unless validator.options.key?(:if) || validator.options.key?(:unless)
           conditional = validator.options.key?(:if) ? validator.options[:if] : validator.options[:unless]
-          
+
           result = if conditional.respond_to?(:call)
             conditional.call(object)
           elsif conditional.is_a?(::Symbol) && object.respond_to?(conditional)
@@ -42,13 +42,13 @@ module Formtastic
           else
             conditional
           end
-          
+
           result = validator.options.key?(:unless) ? !result : !!result
           not_required_through_negated_validation! if !result && [:presence, :inclusion, :length].include?(validator.kind)
 
           result
         end
-        
+
         def validation_limit
           validation = validations? && validations.find do |validation|
             validation.kind == :length
@@ -59,17 +59,17 @@ module Formtastic
             nil
           end
         end
-        
+
         # Prefer :greater_than_or_equal_to over :greater_than, for no particular reason.
         def validation_min
           validation = validations? && validations.find do |validation|
             validation.kind == :numericality
           end
-          
-          if validation 
+
+          if validation
             # We can't determine an appropriate value for :greater_than with a float/decimal column
             raise IndeterminableMinimumAttributeError if validation.options[:greater_than] && column? && [:float, :decimal].include?(column.type)
-            
+
             return validation.options[:greater_than_or_equal_to] if validation.options[:greater_than_or_equal_to]
             return (validation.options[:greater_than] + 1)       if validation.options[:greater_than]
           end
@@ -83,12 +83,12 @@ module Formtastic
           if validation
             # We can't determine an appropriate value for :greater_than with a float/decimal column
             raise IndeterminableMaximumAttributeError if validation.options[:less_than] && column? && [:float, :decimal].include?(column.type)
-            
+
             return validation.options[:less_than_or_equal_to] if validation.options[:less_than_or_equal_to]
             return (validation.options[:less_than] - 1)       if validation.options[:less_than]
           end
         end
-        
+
         def validation_step
           validation = validations? && validations.find do |validation|
             validation.kind == :numericality
@@ -110,33 +110,37 @@ module Formtastic
             false
           end
         end
-        
+
         def validations?
           !validations.empty?
         end
-        
+
         def required?
           return false if options[:required] == false
           return true if options[:required] == true
           return false if not_required_through_negated_validation?
           if validations?
-            validations.select { |validator| 
+            validations.select { |validator|
               [:presence, :inclusion, :length].include?(validator.kind) &&
               validator.options[:allow_blank] != true
             }.any?
           else
-            return !!builder.all_fields_required_by_default
+            return responds_to_global_required? &&!!builder.all_fields_required_by_default
           end
         end
-        
+
+        def responds_to_global_required?
+          true
+        end
+
         def not_required_through_negated_validation?
           @not_required_through_negated_validation
         end
-        
+
         def not_required_through_negated_validation!
           @not_required_through_negated_validation = true
         end
-        
+
         def optional?
           !required?
         end
@@ -150,14 +154,13 @@ module Formtastic
         def column_limit
           column.limit if column? && column.respond_to?(:limit)
         end
-        
+
         def limit
           validation_limit || column_limit
         end
-        
+
       end
     end
   end
 end
-        
-        
+
