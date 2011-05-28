0b5caf8fad7a3c4616f180d54b19c85f437b7b5c
Konstantin Shabanov <etehtsea@gmail.com>
2011-05-28 16:18:04 +0700

    typo fix

--- lib/formtastic/form_builder.rb
+++ lib/formtastic/form_builder.rb
@@ -10,8 +10,8 @@ module Formtastic
     configure :default_text_field_size
     configure :default_text_area_height, 20
     configure :default_text_area_width
-    configure :all_fields_required_by_default, true
-    configure :include_blank_for_select_by_default, false
+    configure :all_fields_required_by_default, false
+    configure :include_blank_for_select_by_default, true
     configure :required_string, proc { Formtastic::Util.html_safe(%{<abbr title="#{Formtastic::I18n.t(:required)}">*</abbr>}) }
     configure :optional_string, ''
     configure :inline_errors, :sentence
