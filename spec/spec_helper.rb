06d456ea7e79b591a03300aa9a95e2f92d290426
Alex Rothenberg <alex@alexrothenberg.com>
2011-09-30 15:24:53 -0400

    added generator specs using ammeter gem

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -10,6 +10,8 @@ require 'action_dispatch'
 require File.expand_path(File.join(File.dirname(__FILE__), '../lib/formtastic/util'))
 require File.expand_path(File.join(File.dirname(__FILE__), '../lib/formtastic'))
 
+require 'ammeter/init'
+
 # Requires supporting files with custom matchers and macros, etc,
 # in ./support/ and its subdirectories in alphabetic order.
 Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].sort.each {|f| require f}
