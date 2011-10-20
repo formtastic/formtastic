e7d3e0f72d775cdaeaf048a1374f6430db4bc93e
Joel Nimety <jnimety@continuity.net>
2011-10-20 14:43:05 -0400

    memoize the selected values array
    Signed-off-by: Joel Nimety <jnimety@continuity.net>

--- spec/inputs/check_boxes_input_spec.rb
+++ spec/inputs/check_boxes_input_spec.rb
@@ -348,7 +348,7 @@ describe 'check_boxes input' do
         item.should_not_receive(:id)
         item.stub!(:custom_value).and_return('custom_value')
         item.should_receive(:custom_value).exactly(3).times
-        @new_post.author.should_receive(:custom_value).exactly(3).times
+        @new_post.author.should_receive(:custom_value).exactly(1).times
         concat(semantic_form_for(@new_post) do |builder|
           concat(builder.input(:author, :as => :check_boxes, :member_value => :custom_value, :collection => [item, item, item]))
         end)
