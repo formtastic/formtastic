749d7de1b83435178daea2f1151bd534569d03dd
Justin French <justin@indent.com.au>
2011-10-26 07:15:26 +1100

    remove deprecated SemanticFormHelper

--- lib/formtastic/helpers.rb
+++ lib/formtastic/helpers.rb
@@ -9,7 +9,6 @@ module Formtastic
     autoload :InputHelper, 'formtastic/helpers/input_helper'
     autoload :InputsHelper, 'formtastic/helpers/inputs_helper'
     autoload :LabelHelper, 'formtastic/helpers/label_helper'
-    autoload :SemanticFormHelper, 'formtastic/helpers/semantic_form_helper'
     autoload :Reflection, 'formtastic/helpers/reflection'
   end
 end
