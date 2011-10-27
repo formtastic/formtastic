0566774dba3419edcea97a54f022d8e62f65e351
Joe Slag <joe@slagwerks.com>
2011-10-27 11:34:38 -0500

    address syntax errors with 1.9.2-p290
    The syntax introduced in 253dc6d559b610f04fab1bc43209d6caa0603d30, eg.
    
      concat(builder.fields_for :author, :index => 3 do |author|
    
    was producing these errors:
    
      syntax error, unexpected keyword_do_block, expecting ')' (SyntaxError)

--- spec/inputs/phone_input_spec.rb
+++ spec/inputs/phone_input_spec.rb
@@ -49,7 +49,7 @@ describe 'phone input' do
       mock_everything
 
       concat(semantic_form_for(@new_post) do |builder|
-        concat(builder.fields_for :author, :index => 3 do |author|
+        concat(builder.fields_for(:author, :index => 3) do |author|
           concat(author.input(:name, :as => :phone))
         end)
       end)
