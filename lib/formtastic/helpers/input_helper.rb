d1667bb15ec18d293794cd270f1cbc1462ba0dd6
Sascha Konietzke <sascha@thriventures.com>
2012-01-26 10:59:24 +0100

    When finding the input class check with const_defined? if a class exists instead of raising exceptions in production environments

--- lib/formtastic/helpers/input_helper.rb
+++ lib/formtastic/helpers/input_helper.rb
@@ -345,6 +345,37 @@ module Formtastic
           end
         end
       end
+      
+      def input_class(as)
+        @input_classes_cache ||= {}
+        @input_classes_cache[as] ||= begin
+          Rails.application.config.cache_classes ? input_class_with_const_defined(as) : input_class_by_trying(as)
+        end
+      end
+      
+      # prevent exceptions in production environment for better performance
+      def input_class_with_const_defined(as)
+        input_class_name = custom_input_class_name(as)
+
+        if ::Object.const_defined?(input_class_name)
+          input_class_name.constantize
+        elsif Formtastic::Inputs.const_defined?(input_class_name)
+          standard_input_class_name(as).constantize 
+        else
+          raise Formtastic::UnknownInputError
+        end
+      end
+      
+      # use auto-loading in development environment
+      def input_class_by_trying(as)
+        begin
+          custom_input_class_name(as).constantize
+        rescue NameError
+          standard_input_class_name(as).constantize
+        end
+      rescue NameError
+        raise Formtastic::UnknownInputError
+      end
 
       # :as => :string # => StringInput
       def custom_input_class_name(as)
