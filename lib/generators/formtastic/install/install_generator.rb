e8f60bbbc6b6a2de65f2b3d10efcbe00664de889
Thomas Walpole <twalpole@gmail.com>
2012-04-16 12:53:32 -0700

    Rails 4 has asset pipeline too

--- lib/generators/formtastic/install/install_generator.rb
+++ lib/generators/formtastic/install/install_generator.rb
@@ -12,8 +12,8 @@ module Formtastic
     source_root File.expand_path('../../../templates', __FILE__)
     class_option :template_engine
 
-    if ::Rails::VERSION::MAJOR == 3 && ::Rails::VERSION::MINOR >= 1
-      # Rails 3.1 has the asset pipeline, no need to copy CSS files any more
+    if ::Rails::VERSION::MAJOR==4 || (::Rails::VERSION::MAJOR == 3 && ::Rails::VERSION::MINOR >= 1)
+      # Rails 3.1+ has the asset pipeline, no need to copy CSS files any more
       desc "Copies a config initializer to config/initializers/formtastic.rb"
       def copy_files
         copy_file 'formtastic.rb', 'config/initializers/formtastic.rb'
