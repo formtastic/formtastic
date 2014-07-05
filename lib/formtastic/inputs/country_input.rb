93d4b0b61a279cc77692739d923ca215745ad85f
Karthik T <karthikt.holmes+github@gmail.com>
2014-07-05 14:40:25 +0800

    Fix country_input to recommend good gem
    This fixes the lack of sync betw. README and actual behavior
    Fixes https://github.com/justinfrench/formtastic/issues/1048
    Currently README requires one gem, but actual exception recommends another

--- lib/formtastic/inputs/country_input.rb
+++ lib/formtastic/inputs/country_input.rb
@@ -51,7 +51,7 @@ module Formtastic
       include Base
 
       def to_html
-        raise "To use the :country input, please install a country_select plugin, like this one: https://github.com/jamesds/country-select" unless builder.respond_to?(:country_select)
+        raise "To use the :country input, please install a country_select plugin, like this one: https://github.com/stefanpenner/country_select" unless builder.respond_to?(:country_select)
         input_wrapping do
           label_html <<
           builder.country_select(method, priority_countries, input_options, input_html_options)
