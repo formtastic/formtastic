a74296a7b74d5bf5710fc9d5de3f6397739909b3
Michal Cichra <michal@o2h.cz>
2014-09-11 13:18:14 +0200

    enable class finders in newly generated apps

--- lib/generators/templates/formtastic.rb
+++ lib/generators/templates/formtastic.rb
@@ -89,9 +89,21 @@
 # See http://diveintohtml5.org/forms.html#validation for more info.
 # Formtastic::FormBuilder.perform_browser_validations = true
 
+# By creating custom input class finder, you can change how input classes  are looked up.
+# For example you can make it to search for TextInputFilter instead of TextInput.
+# See # TODO: add link # for more information
+# NOTE: this behavior will be default from Formtastic 4.0
+Formtastic::FormBuilder.input_class_finder = Formtastic::InputClassFinder
+
 # Define custom namespaces in which to look up your Input classes. Default is
 # to look up in the global scope and in Formtastic::Inputs.
 # Formtastic::FormBuilder.input_namespaces = [ ::Object, ::MyInputsModule, ::Formtastic::Inputs ]
+
+# By creating custom action class finder, you can change how action classes are looked up.
+# For example you can make it to search for MyButtonAction instead of ButtonAction.
+# See # TODO: add link # for more information
+# NOTE: this behavior will be default from Formtastic 4.0
+Formtastic::FormBuilder.action_class_finder = Formtastic::ActionClassFinder
 
 # Define custom namespaces in which to look up your Action classes. Default is
 # to look up in the global scope and in Formtastic::Actions.
