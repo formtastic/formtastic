d39d3f88cf2b438715343112cacaa14e81449ffb
Justin French <justin@indent.com.au>
2015-01-05 13:17:00 +1100

    silence a deprecation we're okay with, see #1080 & #1117
    Cherry-picked from master.
    
    Conflicts:
    	lib/formtastic/inputs/base/database.rb

--- lib/formtastic/inputs/base/database.rb
+++ lib/formtastic/inputs/base/database.rb
@@ -4,7 +4,12 @@ module Formtastic
       module Database
         
         def column
-          object.column_for_attribute(method) if object.respond_to?(:column_for_attribute)
+          if object.respond_to?(:column_for_attribute)
+            # Remove deprecation wrapper & review after Rails 5.0 ships
+            ActiveSupport::Deprecation.silence do
+              object.column_for_attribute(method)
+            end
+          end
         end
         
         def column?
