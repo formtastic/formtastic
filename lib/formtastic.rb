formtastic/formtastic

commit cbbff9c76645be2548f9f6350f5b4b832c4616f0

Remove unnecessary deprecation code for Rails versions
Formtastic is not installable on Rails versions older than 4.1, so all
this deprecation code is dead code.

lib/formtastic.rb
@@ -21,14 +21,6 @@ module Formtastic
     autoload :Util
   end
 
-  # @private
-  mattr_accessor :deprecation
-  self.deprecation = Formtastic::Deprecation.new('4.0', 'Formtastic')
-
-  if defined?(::Rails) && Util.deprecated_version_of_rails?
-    deprecation.warn("Support for Rails < #{Util.minimum_version_of_rails} will be dropped")
-  end
-
   # @public
   class UnknownInputError < NameError
   end

lib/formtastic/util.rb
@@ -1,25 +1,0 @@
-# encoding: utf-8
-
-module Formtastic
-  # @private
-  module Util
-    extend self
-
-    def deprecated_version_of_rails?
-      match?(rails_version, "< #{minimum_version_of_rails}")
-    end
-
-    def minimum_version_of_rails
-      "4.1.0"
-    end
-
-    def rails_version
-      ::Rails::VERSION::STRING
-    end
-
-    def match?(version, dependency)
-      Gem::Dependency.new("formtastic", dependency).match?("formtastic", version)
-    end
-
-  end
-end

spec/spec_helper.rb
@@ -11,7 +11,6 @@ require 'active_record'
 ActiveRecord::Base.establish_connection('url' => 'sqlite3::memory:', 'pool' => 1)
 load 'spec/schema.rb'
 
-require File.expand_path(File.join(File.dirname(__FILE__), '../lib/formtastic/util'))
 require File.expand_path(File.join(File.dirname(__FILE__), '../lib/formtastic'))
 
 #rspec-rails 2-14 assumes a full rails install

spec/util_spec.rb
@@ -1,38 +1,0 @@
-# encoding: utf-8
-require 'spec_helper'
-
-RSpec.describe 'Formtastic::Util' do
-
-  describe '.deprecated_version_of_rails?' do
-
-    subject { Formtastic::Util.deprecated_version_of_rails? }
-
-    context '4.1.0' do
-      before { allow(Formtastic::Util).to receive(:rails_version) { "4.1.0" } }
-      it 'should be false' do
-        expect(subject).to be_falsey
-      end
-    end
-
-    context '4.1.1' do
-      before { allow(Formtastic::Util).to receive(:rails_version) { "4.1.1" } }
-      it 'should be false' do
-        expect(subject).to be_falsey
-      end
-    end
-
-    context '4.2.0' do
-      before { allow(Formtastic::Util).to receive(:rails_version) { "4.2.0" } }
-      it 'should be false' do
-        expect(subject).to be_falsey
-      end
-    end
-
-    context '5.0.0' do
-      before { allow(Formtastic::Util).to receive(:rails_version) { "5.0.0" } }
-      it 'should be true' do
-        expect(subject).to be_falsey
-      end
-    end
-  end
-end

commit 033b84ed476252723c2c5b4d1e91db8cef391589

Fix eager loading problem
Caused by cbbff9c7 which removed `formtastic/util.rb` but not the corresponding `autoload :Util`

lib/formtastic.rb
@@ -18,7 +18,6 @@ module Formtastic
     autoload :FormBuilder
     autoload :Inputs
     autoload :Actions
-    autoload :Util
   end
 
   # @public