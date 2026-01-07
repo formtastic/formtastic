79b1cf387db8f3c1a9094a1a2dad05cf48af46c3
Geremia Taglialatela <tagliala.dev@gmail.com>
2026-01-07 17:42:57 +0100

    Improve CI
    Appraisal:
    - Treat minor Rails releases as major versions
    - Test against latest stable maintained versions of Rails
    
    GitHub Actions:
    - Update actions
    - Test against Ruby 3.4 and 4.0
    - Use strings for Ruby version to avoid issues when 4.1 will be released
    
    Specs:
    - Replace `OrderedHash.new(hash)` call with `OrderedHash[hash]` to fix
      an `ArgumentError` on Ruby >= 3.4
    
    Integration:
    - Run integration tests against latest Rails 8.1
    - Add more flags to improve test application generation
    - Use propshaft to load Formtastic CSS

--- script/integration.sh
+++ script/integration.sh
@@ -6,17 +6,24 @@ set -o verbose
 test_app=dummy
 rm -rf ${test_app}
 
-export RAILS_INTEGRATION_VERSION="8.0.2"
+export RAILS_INTEGRATION_VERSION="8.1.1"
 gem install rails -v ${RAILS_INTEGRATION_VERSION}
 rails -v
 
 rails new ${test_app} \
   --template=$(dirname "$0")/integration-template.rb \
   --skip-bootsnap \
+  --skip-brakeman \
+  --skip-bundler-audit \
+  --skip-ci \
+  --skip-git \
   --skip-javascript \
+  --skip-jbuilder \
+  --skip-kamal \
+  --skip-rubocop \
+  --skip-solid \
   --skip-spring \
-  --skip-turbolinks \
-  --skip-kamal
+  --skip-thruster
 
 cd ${test_app}
 
